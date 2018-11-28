97e85e34b695762c90af5cc0651863a2d69f0e16
ivnod <vinodreddymedagam@gmail.com>
2018-11-29 01:11:58 +0700

    Running on device

--- app/src/main/java/com/learning/vinod/mdnavigationdrawer/MainActivity.java
+++ app/src/main/java/com/learning/vinod/mdnavigationdrawer/MainActivity.java
@@ -1,19 +1,30 @@
 package com.learning.vinod.mdnavigationdrawer;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
 
 public class MainActivity extends AppCompatActivity {
 
     Toolbar mToolbar;
+    DrawerLayout mDrawerLayout;
+    NavigationView mNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.navigation_drawer);
-        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        mToolbar = findViewById(R.id.toolbar);
         setSupportActionBar(mToolbar);
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+        mNavigationView = findViewById(R.id.navigation_view);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, mDrawerLayout, mToolbar, R.string.open_drawer, R.string.close_drawer);
+        mDrawerLayout.setDrawerListener(toggle);
+        toggle.syncState();
+
     }
 }
