3a7e8750f7e9642461e03bf04a184373298b9f1e
ivnod <vinodreddymedagam@gmail.com>
2018-11-29 09:33:39 +0700

    1. Making drawer layout 2. include main layout inside drawer layout 3. add Navigation view to drawer layout ,headerLayout,menu tags 4. make navigation header 5. layout_gravity, gravity,scaleType,textAppearance,include,fitsSystmeWindow 6. menu, group, menu inside item, checkable behaviour 7. openDrawer property 8. ActionBarToggle class, set listener, syncState 9. Adding action listener to navigation view 10. close drawer on onclick of Back 11. Make image a circle view using library 12. windowActionBar, windowNoTitle,statusBarColor,windowDrawsSystemBarBackgrounds 13.

--- app/src/main/java/com/learning/vinod/mdnavigationdrawer/MainActivity.java
+++ app/src/main/java/com/learning/vinod/mdnavigationdrawer/MainActivity.java
@@ -1,14 +1,18 @@
 package com.learning.vinod.mdnavigationdrawer;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     Toolbar mToolbar;
     DrawerLayout mDrawerLayout;
@@ -22,9 +26,52 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(mToolbar);
         mDrawerLayout = findViewById(R.id.drawer_layout);
         mNavigationView = findViewById(R.id.navigation_view);
+        mNavigationView.setNavigationItemSelectedListener(this);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, mDrawerLayout, mToolbar, R.string.open_drawer, R.string.close_drawer);
         mDrawerLayout.setDrawerListener(toggle);
         toggle.syncState();
 
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (mDrawerLayout.isDrawerOpen(Gravity.START)) {
+            mDrawerLayout.closeDrawer(Gravity.START);
+        } else {
+            super.onBackPressed();
+        }
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        int id = menuItem.getItemId();
+        switch (id) {
+            case R.id.inbox:
+                Toast.makeText(this, "inbox", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.starred:
+                Toast.makeText(this, "starred", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.sent_mail:
+                Toast.makeText(this, "sent mail", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.drafts:
+                Toast.makeText(this, "drafts", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.all_mail:
+                Toast.makeText(this, "all mail", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.spam:
+                Toast.makeText(this, "spam", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.delete:
+                Toast.makeText(this, "delete", Toast.LENGTH_LONG).show();
+                break;
+
+
+        }
+        mDrawerLayout.closeDrawer(Gravity.START);
+        return true;
     }
 }
